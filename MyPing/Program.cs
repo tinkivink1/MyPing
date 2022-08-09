0233f446c0b178f9a482424dbeff8938560add7e
Admin <Admin@DEV88>
2022-08-09 09:47:20 +0300

    Добавьте файлы проекта.

---
+++ MyPing/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Text;
+using System.Net.Sockets;
+using System.Collections;
+
+string ip;
+int packetSize;
+Icmp icmp = new Icmp();
+Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+
+
+try
+{
+    ip = "8.8.8.8";
+    packetSize = 1024;
+    //ip = args[0];
+    //packetSize = int.Parse(args[1]);
+    icmp.messageSize = packetSize;
+    host.Connect(System.Net.IPAddress.Parse(ip), 0);
+}
+catch(Exception ex) when (ex is ArgumentNullException || ex is OverflowException || ex is FormatException)
+{
+    Console.WriteLine("Invalid command line arguments\n" +
+                        "\tMyPing [IP] [PING]");
+    return -1; 
+}
+catch(Exception ex) when (ex is SocketException)
+{
+    Console.WriteLine("Connection error\n");
+    return -1;
+}
+
+
+while (true)
+{
+    byte[] TTL = new byte[1]; 
+    byte[] buffer = new byte[packetSize];
+    byte[] sourceAddress = new byte[4];
+    byte[] destinationAddress = new byte[4];
+
+    icmp.checkSum = icmp.getChecksum();
+    host.Send(icmp.getBytes());
+
+    DateTime sentTime = DateTime.Now;
+    var recievedBytes = host.Receive(buffer);
+    DateTime recievedTime = DateTime.Now;
+
+    Buffer.BlockCopy(buffer, 8, TTL, 0, 1);
+    Buffer.BlockCopy(buffer, 12, destinationAddress, 0, 4);
+    Buffer.BlockCopy(buffer, 16, sourceAddress, 0, 4);
+    Buffer.BlockCopy(buffer, 24, buffer, 0, recievedBytes);
+
+    Console.WriteLine($"Response from {string.Join(".", destinationAddress)}: size = {packetSize}, ping = {(int)(recievedTime - sentTime).TotalMilliseconds} ms, TTL = {TTL[0]}");
+    Thread.Sleep(500);
+}
+
+
+struct Icmp
+{
+    public byte type;
+    public byte code;
+    public Int16 checkSum;
+    public int messageSize;
+    public byte[] message = new byte[1024];
+
+    public Icmp()
+    {
+        type = 0x08;
+        code = 0x00;
+        checkSum = 0x00;
+        messageSize = 0;
+    }
+
+    public Icmp(string data)
+    {
+        type = 0x08;
+        code = 0x00;
+        checkSum = 0x00;
+        message = Encoding.UTF8.GetBytes(data);
+        messageSize = Encoding.UTF8.GetBytes(data).Length;
+    }
+
+    public Icmp(byte[] data, int size)
+    {
+        type = data[20];
+        code = data[21];
+        checkSum = BitConverter.ToInt16(data, 22);
+        messageSize = size - 24;
+        Buffer.BlockCopy(data, 24, message, 0, messageSize);
+    }
+
+    public byte[] getBytes()
+    {
+        byte[] data = new byte[messageSize + 9];
+        Buffer.BlockCopy(BitConverter.GetBytes(type), 0, data, 0, 1);
+        Buffer.BlockCopy(BitConverter.GetBytes(code), 0, data, 1, 1);
+        Buffer.BlockCopy(BitConverter.GetBytes(checkSum), 0, data, 2, 2);
+        Buffer.BlockCopy(message, 0, data, 4, messageSize);
+        return data;
+    }
+
+    public Int16 getChecksum()
+    {
+        checkSum = 0;
+        UInt32 checksum = 0;
+        byte[] data = getBytes();
+        int currentBytes = 0;
+
+        while (currentBytes < messageSize + 8)
+        {
+            checksum += Convert.ToUInt32(BitConverter.ToUInt16(data, currentBytes));
+            currentBytes += 2;
+        }
+
+        checksum = (checksum >> 16) + (checksum);
+        return (Int16)(~checksum);
+    }
+
+    public void setSize(int size)
+    {
+        for(int i = 0; i < size - 4; i++)
+        {
+            message[i] = (byte)(i % 255);
+        }
+        messageSize = size;
+    }
+}
---
+++ temp/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+bool ConnectedToRover;
+string roverIPAddress = "127.0.0.1";
+long PingRTT;
+
+
+
+Ping();
+
+void Ping()
+{
+    byte[] data = new byte[1024];
+    int recv;
+    Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+    IPEndPoint iep = new IPEndPoint(IPAddress.Parse(roverIPAddress), 0); //ICMP doesn't use port numbers
+    EndPoint ep = (EndPoint)iep;
+    ICMP packet = new ICMP();
+    packet.Type = 0x08;
+    packet.Code = 0x00;
+    packet.Checksum = 0;
+    Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 0, 2);
+    Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 2, 2);
+    data = Encoding.ASCII.GetBytes("pingping");
+    Buffer.BlockCopy(data, 0, packet.Message, 4, data.Length);
+    packet.MessageSize = data.Length + 4;
+    int packetsize = packet.MessageSize + 4;
+    UInt16 chcksum = packet.getChecksum();
+    packet.Checksum = chcksum;
+    host.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 1000);
+
+    while (true)
+    {
+        DateTime sentAt = DateTime.Now;
+        DateTime recvdAt = sentAt;
+        host.SendTo(packet.getBytes(), packetsize, SocketFlags.None, iep);
+        try
+        {
+            data = new byte[1024];
+            recv = host.ReceiveFrom(data, ref ep);
+            recvdAt = DateTime.Now;
+            ICMP response = new ICMP(data, recv);
+            Console.WriteLine(Encoding.UTF8.GetString(response.Message));
+            DateTime recvd = DateTime.Now;
+            PingRTT = (long)(recvdAt - sentAt).TotalMilliseconds;
+        }
+        catch (SocketException)
+        {
+            PingRTT = 0;
+        }
+
+        Thread.Sleep(500);
+    }
+}
+
+
+//From "C# Network Programming" by Richard Blum
+class ICMP
+{
+
+    public UInt16 Checksum;
+    public byte Code;
+    public byte[] Message = new byte[1024];
+    public int MessageSize;
+    public byte Type;
+
+
+    public ICMP()
+    {
+    }
+
+    public ICMP(byte[] data, int size)
+    {
+        Type = data[20];
+        Code = data[21];
+        Checksum = BitConverter.ToUInt16(data, 22);
+        MessageSize = size - 24;
+        Buffer.BlockCopy(data, 24, Message, 0, MessageSize);
+    }
+
+
+
+    public byte[] getBytes()
+    {
+        byte[] data = new byte[MessageSize + 9];
+        Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+        Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, data, 1, 1);
+        Buffer.BlockCopy(BitConverter.GetBytes(Checksum), 0, data, 2, 2);
+        Buffer.BlockCopy(Message, 0, data, 4, MessageSize);
+        return data;
+    }
+
+    public UInt16 getChecksum()
+    {
+        UInt32 chcksm = 0;
+        byte[] data = getBytes();
+        int packetsize = MessageSize + 8;
+        int index = 0;
+
+        while (index < packetsize)
+        {
+            chcksm += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+            index += 2;
+        }
+        chcksm = (chcksm >> 16) + (chcksm & 0xffff);
+        chcksm += (chcksm >> 16);
+        return (UInt16)(~chcksm);
+    }
+
+}
